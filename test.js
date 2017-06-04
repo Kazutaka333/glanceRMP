eeed31c36412ac14eedc95aa78e15f98696496d8
Kazutaka Homma <special18fender@gmail.com>
2017-06-04 15:24:14 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,25 @@
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function makeQuery(name, college)
+{
+    var query = "query=";
+    name = name.replace(" ", "+");
+    college = college.replace(" ", "+");
+    query += name + "+" + college;
+    return query;
+}
+
+// http://www.ratemyprofessors.com/search.jsp?query=Derrick+Smith+Laney 
+var baseURL = "http://www.ratemyprofessors.com/search.jsp?";
+var name = "Derrick Smith";
+
+httpGetAsync(baseURL+makeQuery(name, "Laney");
