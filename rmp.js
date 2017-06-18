8ecd929ed2ba3333658aa1e7bb8c1baa849c45f1
Kazutaka Homma <special18fender@gmail.com>
2017-06-18 00:41:04 -0700

    add class for search for prof on rmp

---
+++ rmp.js
@@ -0,0 +1,26 @@
+/*
+ File name: rmp.js
+ Description: this file contains functions that find data from RMP and return
+              formatted data.
+ */
+
+
+// RMPQuery class
+function RMPQuery (college, profName) {
+    this.college = college;
+    this.profName = profName;
+}
+
+
+// RMPSearch class
+function RMPSearch (query) {
+    this.query = query;
+    this.url = "";
+    this.rate = "";
+    this.name = "";
+
+    function search() {
+        // search with query and set above member variables
+    }
+    search();
+}
